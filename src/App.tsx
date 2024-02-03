603c289cbaf6aebdf0705853e6deddf8f6b27aff
Sainul Abid <sainulabidofficial@gmail.com>
2024-02-03 07:28:32 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Detail from "./components/Detail";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Detail />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <div className="text-7xl">Header</div>;
+}
+
+export default Header;
